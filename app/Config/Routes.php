2cdef48d32ba60ba3f93c0a20239190810ee76b8
Rafi Abiyyu <rafiristi@gmail.com>
2024-07-15 11:57:00 +0700

    menambah data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,20 +5,53 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
 
 $routes->get('login', 'AuthController::login');
-$routes->post('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login', ['filter' => 'redirect']);
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
-$routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
-$routes->post('produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
-$routes->get('produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
-$routes->get('download', 'ProdukController::download');
+$routes->group('produk', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'ProdukController::index', ['filter' => 'auth']);
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+    $routes->get('download', 'ProdukController::download');
+});
+
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+});
+
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->get('getcity', 'TransaksiController::getcity', ['filter' => 'auth']);
+$routes->get('getcost', 'TransaksiController::getcost', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
 
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
 $routes->get('faq', 'Home::faq', ['filter' => 'auth']);
-$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('contact', 'Home::contact', ['filter' => 'auth']);
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
+
+$routes->group('api', function ($routes) {
+    $routes->post('monthly', 'ApiController::monthly');
+    $routes->post('yearly', 'ApiController::yearly');
+});
+
+// $routes->group('api', function ($routes) {
+//     $routes->post('yearly', 'ApiController::yearly');
+// });
+    
+    // transaksi
+$routes->group('transaksi', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::transaksi');
+    $routes->post('updateStatus', 'TransaksiController::updateStatus');
+    $routes->get('downloadTransaksi', 'TransaksiController::downloadTransaksi');
+});
--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controllers;
 
+namespace App\Controllers;
+
 use App\Models\ProductModel;
+use Dompdf\Dompdf;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
 
 class ProdukController extends BaseController
 {
@@ -14,6 +20,7 @@ class ProdukController extends BaseController
         $this->product = new ProductModel();
     }
 
+
     public function index()
     {
         $product = $this->product->findAll();
@@ -23,66 +30,120 @@ class ProdukController extends BaseController
     }
 
     public function create()
-{
-    $dataFoto = $this->request->getFile('foto');
+    {
+        $dataFoto = $this->request->getFile('foto');
 
-    $dataForm = [
-        'nama' => $this->request->getPost('nama'),
-        'harga' => $this->request->getPost('harga'),
-        'jumlah' => $this->request->getPost('jumlah'),
-        'created_at' => date("Y-m-d H:i:s")
-    ];
+        $dataForm = [
+            'nama' => $this->request->getPost('nama'),
+            'harga' => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah'),
+            'created_at' => date("Y-m-d H:i:s")
+        ];
 
-    if ($dataFoto->isValid()) {
-        $fileName = $dataFoto->getRandomName();
-        $dataForm['foto'] = $fileName;
-        $dataFoto->move('img/', $fileName);
+                // Validation rules
+        $rules = [
+            'nama' => 'required|min_length[6]',
+            'harga' => 'required|numeric',
+            'jumlah' => 'required|numeric'
+        ];  
+        
+        // Validate the input
+        if ($this->validate($rules)) {
+
+        if ($dataFoto->isValid()) {
+            $fileName = $dataFoto->getRandomName();
+            $dataForm['foto'] = $fileName;
+            $dataFoto->move('img/', $fileName);
+        }
+
+        $this->product->insert($dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Ditambah');
+        } else {
+                    // Validation failed, show errors
+                session()->setFlashdata('failed', $this->validator->listErrors());
+                return redirect()->back();
+            }
     }
 
-    $this->product->insert($dataForm);
+    public function edit($id)
+    {
+        $dataProduk = $this->product->find($id);
 
-    return redirect('produk')->with('success', 'Data Berhasil Ditambah');
-} 
-public function edit($id)
-{
-    $dataProduk = $this->product->find($id);
+        $dataForm = [
+            'nama' => $this->request->getPost('nama'),
+            'harga' => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah'),
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
 
-    $dataForm = [
-        'nama' => $this->request->getPost('nama'),
-        'harga' => $this->request->getPost('harga'),
-        'jumlah' => $this->request->getPost('jumlah'),
-        'updated_at' => date("Y-m-d H:i:s")
-    ];
+        // Validation rules
+        $rules = [
+            'nama' => 'required|min_length[6]',
+            'harga' => 'required|numeric',
+            'jumlah' => 'required|numeric'
+        ];  
 
-    if ($this->request->getPost('check') == 1) {
+        // Validate the input
+        if ($this->validate($rules)) {
+            if ($this->request->getPost('check') == 1) {
+                if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
+                    unlink("img/" . $dataProduk['foto']);
+                }
+
+                $dataFoto = $this->request->getFile('foto');
+
+                if ($dataFoto->isValid()) {
+                    $fileName = $dataFoto->getRandomName();
+                    $dataFoto->move('img/', $fileName);
+                    $dataForm['foto'] = $fileName;
+                }
+            }
+
+            $this->product->update($id, $dataForm);
+
+            return redirect('produk')->with('success', 'Data Berhasil Diubah');
+        } else {
+                    // Validation failed, show errors
+                session()->setFlashdata('failed', $this->validator->listErrors());
+                return redirect()->back();
+            }
+    }
+
+    public function delete($id)
+    {
+        $dataProduk = $this->product->find($id);
+
         if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
             unlink("img/" . $dataProduk['foto']);
         }
 
-        $dataFoto = $this->request->getFile('foto');
+        $this->product->delete($id);
 
-        if ($dataFoto->isValid()) {
-            $fileName = $dataFoto->getRandomName();
-            $dataFoto->move('img/', $fileName);
-            $dataForm['foto'] = $fileName;
-        }
+        return redirect('produk')->with('success', 'Data Berhasil Dihapus');
     }
 
-    $this->product->update($id, $dataForm);
+    public function download()
+    {
+        $product = $this->product->findAll();
 
-    return redirect('produk')->with('success', 'Data Berhasil Diubah');
-}
+        $html = view('v_produkPDF', ['product' => $product]);
 
-public function delete($id)
-{
-    $dataProduk = $this->product->find($id);
+        $filename = date('y-m-d-H-i-s') . '-produk';
 
-    if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
-        unlink("img/" . $dataProduk['foto']);
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+
+        // load HTML content
+        $dompdf->loadHtml($html);
+
+        // (optional) setup the paper size and orientation
+        $dompdf->setPaper('A4', 'potrait');
+
+        // render html as PDF
+        $dompdf->render();
+
+        // output the generated pdf
+        $dompdf->stream($filename);
     }
-
-    $this->product->delete($id);
-
-    return redirect('produk')->with('success', 'Data Berhasil Dihapus');
-}
 }
